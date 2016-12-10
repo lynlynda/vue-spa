5613a27984ae2127039b2ed7db16e196fa132406
高琳 <gaolin@gaolindeMacBook-Pro.local>
2016-12-10 15:29:14 +0800

    common commit

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -6,7 +6,7 @@
         <div class="desc">
           <star :size="36" :score="seller.score"></star>
           <span class="text">({{seller.ratingCount}})</span>
-          <span class="text">{{seller.sellCount}}</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
         </div>
         <ul class="remark">
           <li class="block">
@@ -29,11 +29,25 @@
           </li>
         </ul>
       </div>
+      <split></split>
+      <div class="bulletin">
+        <h class="title">公告与活动</h>
+        <div class="content-wrapper">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li v-for="support in seller.supports" class="support-item border-1px">
+            <span :class="classMap[support.type]" class="icon"></span>
+            <span class="text">{{support.description}}</span>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 <script type='text/javascript'>
 import star from 'components/star/star';
+import split from 'components/split/split';
   export default {
     props: {
       seller: {
@@ -41,11 +55,15 @@ import star from 'components/star/star';
       }
     },
     components: {
-      star
+      star, split
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     }
   };
 </script>
 <style lang='stylus' rel='stylssheet/stylus'>
+  @import '../../common/stylus/mixin'
   .seller
     position: fixed
     top: 174px
@@ -58,13 +76,59 @@ import star from 'components/star/star';
       .title
         font-size: 14px
         margin-bottom: 8px
-        line-height: 14
+        line-height: 14px
         color: rgb(7,17,27)
       .desc
         padding-bottom: 18px
-        line-height: 18px
         font-size: 0
-        
+        border-1px(rgba(7,17,27,0.1))
+        .star
+          display: inline-block
+          vertical-align: top
+          margin-right: 8px
+        .text
+          display: inline-block
+          line-height: 18px
+          vertical-align: top
+          margin-right: 12px
+          font-size: 10px
+          color: rgb(77,85,93);
+      .remark
+        display: flex
+        padding-top: 18px
+        .block
+          flex: 1
+          text-align: center
+          border-right: 1px solid rgba(7,17,27,0.1)
+          &:last-child
+            border: none
+          h2
+            margin-bottom: 4px
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147,153,159)
+          .content
+            line-height: 24px
+            font-size: 10px
+            color: rgb(7,17,27)
+            .stress
+              font-size: 24px
+    .bulletin
+      padding: 18px 18px 0 18px
+      .title
+        font-size: 14px
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7,17,27)
+      .content-wrapper
+        padding: 0 12px 16px 12px
+        border-1px(rgba(7,17,27,0.1))
+        .content
+          line-height: 24px
+          font-size: 12px
+          color: rgb(240,20,20)
+
+
 
 
 
